8c045dc5a0ad0fc4cc1f7167eb8b0e4def8fdf71
Bradley Holbrook <bholbrook73@gmail.com>
2020-09-15 18:00:51 -0700

    Saying hello to mars

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const condition = 1;
 
 if (condition) {
-    console.log("Hello World");
+    console.log("Hello Mars!");
 } else {
     console.log("Goodbye Cruel World");
     console.log("Goodbye World!");
