b8972520f1e2f6510c20b2165e3f8c3137597d01
Bradley Holbrook <bholbrook73@gmail.com>
2020-09-15 17:59:30 -0700

    Added spacing, and said hello louder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 const condition = 1;
 
 if (condition) {
-    console.log("Hello World");
+    
+    console.log("Hello World!");
+
 } else {
+    
     console.log("Goodbye Cruel World");
     console.log("Goodbye World!");
+
 }
